88d7449769a29b88725e4bf89da1035d3a33edb7
NadiasCodes <De.nadia@outlook.com>
2024-02-28 16:16:54 +0100

    added and customized swiper

---
+++ src/java.js
@@ -0,0 +1,28 @@
+// Swipper
+
+let swiperProject = new Swiper(".projects-container", {
+  loop: true,
+  spaceBetween: 480,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 1,
+      spaceBetween: 480,
+    },
+  },
+});
